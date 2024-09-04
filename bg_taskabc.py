7e6628748ff185d17e4300ea987206f3511289c4
Benjamin <benjagr@uio.no>
2024-09-04 19:40:46 +0200

    BG- Task a finished

--- bg_taskabc.py
+++ bg_taskabc.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+from random import random, seed
 
+np.random.seed(1)
 
 def MSE(y_data, y_model):
     n = np.size(y_model)
@@ -12,154 +19,215 @@ def MSE(y_data, y_model):
 def R2(y_data, y_model):
     return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
 
-
-
-
-np.random.seed(1)
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import numpy as np
-from random import random, seed
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-# Make data.
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05)
-x, y = np.meshgrid(x,y)
-
-
 def FrankeFunction(x,y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
+        term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+        term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+        term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+        term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+        return term1 + term2 + term3 + term4
+
+def Design_Matrix_2D(deg, X):
+    # The number of polynomial terms for two variables (x, y) up to degree d is (d+1)(d+2)/2  
+    # Minus 1 from dropping intercept-column
+    num_terms = int((deg + 1) * (deg + 2) / 2 - 1)
+
+    Phi = np.zeros((X.shape[0], num_terms))
+    # PS: not include intercept in design matrix, will scale (centered values)
+    col = 0
+    for dx in range(1,deg + 1):
+        for dy in range(dx + 1):
+            # X[:,0] = x-values
+            # X[:,1] = y-values
+            Phi[:,col] = (X[:,0] ** (dx - dy)) * (X[:,1] ** dy)
+            col += 1
+    return Phi
 
 
-z = FrankeFunction(x, y)
+def test_1D():
+    fig, ax = plt.subplots()
 
-# Plot the surface.
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.savefig('FrankeFunction.png')
-plt.show()
+    # Make data.
+    x = np.arange(0, 1, 0.05)
+    y = 1 # Testing one dimentional
+    
 
 
+    z = FrankeFunction(x, y)
+
+    # Making Design Matrix
+    deg = 5 #degree of polynomial
+
+    X = np.zeros((len(x), deg))
+    # PS: not include intercept in design matrix, will scale (centered values)
+    col = 0
+    for dx in range(1,deg + 1):
+        X[:,col] = (x ** dx)
+        col += 1
+        
+
+    # Split data in training and test data
+    X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.25) 
+    # give: test 1/4 of data,  train = 3/4 af data
 
 
-
-
-n = 100
-x = np.linspace(-3, 3, n).reshape(-1, 1)
-y = np.exp(-x**2) + 1.5 * np.exp(-(x-2)**2)+ np.random.normal(0, 0.1, x.shape)
-
-# Defining Design matrix (The Vermonde-Matrix) 
-deg = 5 #degree of polynomial
-
-
-X = np.zeros((len(x), deg))
-# PS: not include intercept in design matrix, will scale (centered values)
-for d in range(deg):
-    X[:,d] = x.flatten() ** (d+1)
-
-
-# Split data in training and test data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) 
-# give: test 1/5 of data,  train = 4/5 af data
-
-# Model Training
-X_train_mean = np.mean(X_train)
-y_train_mean = np.mean(y_train)
-X_train_scaled = X_train - X_train_mean
-y_train_scaled = y_train - y_train_mean
-# Model predictions
-X_test_scaled = X_test - X_train_mean
+    # Model Training
+    X_train_mean = np.mean(X_train)
+    z_train_mean = np.mean(z_train)
+    X_train_scaled = X_train - X_train_mean
+    z_train_scaled = z_train - z_train_mean
+    # Model predictions
+    X_test_scaled = X_test - X_train_mean
 
 
 
-# ------------------------- MANUALLY OLS ---------------------------------------------
-OLSbeta = np.linalg.inv(X_train_scaled.T @ X_train_scaled) @ X_train_scaled.T @ y_train_scaled
-y_train_OLS = X_train_scaled @ OLSbeta + y_train_mean # adding intercepts
+    # ------------------------- MANUALLY OLS ---------------------------------------------
+    # Note: must use PINV, given 1st row = 0, which give det(X) = 0 -> X is Singular Matrix
+    # Note: use psudoinverse 
+    OLSbeta = np.linalg.pinv(X_train_scaled.T @ X_train_scaled) @ X_train_scaled.T @ z_train_scaled
+    z_train_OLS = X_train_scaled @ OLSbeta + z_train_mean # adding mean_value
+
+    z_test_OLS = X_test_scaled @ OLSbeta + z_train_mean # adding mean_value
 
 
-y_test_OLS = X_test_scaled @ OLSbeta + y_train_mean # adding intercepts
+    # Sort the training and test data and corresponding predictions
+    sorted_indices = np.argsort(X_train_scaled[:,0].flatten())
+    X_train_sort = X_train_scaled[sorted_indices]
+    z_train_OLS_sort = z_train_OLS[sorted_indices]
+    z_train_sort = z_train_scaled[sorted_indices]
+
+    sorted_indices = np.argsort(X_test_scaled[:,0].flatten())
+    X_test_sort = X_test_scaled[sorted_indices]
+    z_test_OLS_sort = z_test_OLS[sorted_indices]
+    z_test_sort = z_test[sorted_indices]
+
+    # Finding values for plotting (Fist column in Design-Matrix X)
+    # Adding mean-value for plotting
+    x_train = X_train_sort[:,0] + X_train_mean 
+    x_test = X_test_sort[:,0] + X_train_mean
 
 
-# Sort the training and test data and corresponding predictions
-sorted_indices = np.argsort(X_train_scaled[:,0].flatten())
-X_train_sort = X_train_scaled[sorted_indices]
-y_train_OLS_sort = y_train_OLS[sorted_indices]
-y_train_sort = y_train_scaled[sorted_indices]
+    ax.set_title(f'OLS Regression, Polynomial Degree = {deg}')
 
-sorted_indices = np.argsort(X_test_scaled[:,0].flatten())
-X_test_sort = X_test_scaled[sorted_indices]
-y_test_OLS_sort = y_test_OLS[sorted_indices]
-y_test_sort = y_test[sorted_indices]
+    # Plot the surface.
+    surf = ax.plot(x, z, label='FrankeFunction') 
+    reg = ax.plot(x_train, z_train_OLS_sort, label='OLS Regression')
 
-
-# Finding values for plotting (Fist column in Design-Matrix X)
-# Adding mean-value for plotting
-x_train = X_train_sort[:,0] + X_train_mean 
-x_test = X_test_sort[:,0] + X_train_mean
+    ax.legend()
+    ax.set_ylim(-0.10, 1.40)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
 
 
-fig, [ax1, ax2] = plt.subplots(1, 2,figsize = (15,8))
-fig.suptitle(f'Polynomial Degree {deg}', fontsize=16)
-ax1.set_title(f'OLS Regression (Manually)')
-ax1.plot(x,y, 'bo', ms = 5, label='data')
-ax1.plot(x_train, y_train_OLS_sort, 'r', label='Regression Training')
-ax1.plot(x_test, y_test_OLS_sort, 'g', label='Regression Test')
-ax1.legend()
-plt.plot()
-plt.savefig(f'OLS.png')
+    # Add a color bar which maps values to colors.
+    plt.savefig('FrankeFunction_1D_regression.png')
+    plt.show()
 
 
-"""
-# ----------------------------- USING SICKIT-LEARN------------------------------
-# not included intercept in design matrix X
-linreg = LinearRegression(fit_intercept=False).fit(X_train, y_train)
+
+    # ----------------------------- MSE ---------------------
+    MSETrain_OLS = MSE(z_train_sort + z_train_mean, z_train_OLS_sort)
+    MSETest_OLS = MSE(z_test_sort, z_test_OLS_sort)
+
+    R2Train_OLS = R2(z_train_sort + z_train_mean, z_train_OLS_sort)
+    R2Test_OLS = R2(z_test_sort, z_test_OLS_sort)
+
+    print(f'Polynomial Degree {deg}')
+    print(f"Training MSE for OLS = {MSETrain_OLS:.2e} \nTest MSE OLS = {MSETest_OLS:.2e} \n")
+    print(f"Training R2 for OLS = {R2Train_OLS:.2e} \nTest R2 OLS = {R2Test_OLS:.2e}")
 
 
-y_train_OLS = linreg.predict(X_train_sort) + y_train_mean
-y_test_OLS = linreg.predict(X_test_sort) + y_train_mean
+def test_2D():
+
+    # Make data.
+    x = np.arange(0, 1, 0.05)
+    y = np.arange(0, 1, 0.05)
+    x, y = np.meshgrid(x,y)
+
+    z = FrankeFunction(x, y)
 
 
-#ax2 = fig.add_subplot(2,1,2)
-ax2.set_title('OLS Regression (Scikit-Learn)')
-ax2.plot(x,y, 'bo', ms = 5, label='data')
-ax2.plot(x_train, y_train_OLS, 'r', label='Regression Training')
-ax2.plot(x_test, y_test_OLS, 'g', label='Regression Test')
-ax2.legend()
-plt.plot()
-plt.savefig(f'uke_36_fig2{deg}.png')
-print()
+    x_flat = x.flatten()
+    y_flat = y.flatten()
+    z_flat = z.flatten()
 
-"""
 
-# ----------------------------- MSE ---------------------
-MSETrain_OLS = MSE(y_train_sort + y_train_mean, y_train_OLS_sort)
-MSETest_OLS = MSE(y_test_sort, y_test_OLS_sort)
+    X = np.vstack((x_flat,y_flat)).T
+    # X[:,0] = x-vals  X[:,1] = y-vals
 
-R2Train_OLS = R2(y_train_sort + y_train_mean, y_train_OLS_sort)
-R2Test_OLS = R2(y_test_sort, y_test_OLS_sort)
+    X_train, X_test, z_train, z_test = train_test_split(X, z_flat, test_size=0.25)
 
-print(f'Polynomial Degree {deg}')
-print(f"Training MSE for OLS = {MSETrain_OLS:.2f} \nTest MSE OLS = {MSETest_OLS:.2f} \n")
-print(f"Training R2 for OLS = {R2Train_OLS:.2f} \nTest R2 OLS = {R2Test_OLS:.2f}")
+    #----------------------------------------   Scaling -----------------------------
+    X_train_mean = np.mean(X_train) 
+    z_train_mean = np.mean(z_train)
+    X_train -= X_train_mean
+    z_train -= z_train_mean
+    X_test -= X_train_mean
+
+
+    # Design Matrix Phi
+    deg = 5 #degree of polynomial
+    Phi_train = Design_Matrix_2D(deg, X_train)
+    Phi_test = Design_Matrix_2D(deg, X_test)
+
+
+    #-----------------------------------------   OLS    -----------------------------
+    OLSbeta = np.linalg.inv(Phi_train.T @ Phi_train) @ Phi_train.T @ z_train
+    z_pred = Phi_test @ OLSbeta
+    z_tilde = Phi_train @ OLSbeta
+
+    #----------------------------------------- Plotting -------------------
+    # Reshape for plotting
+    # Adding mean values for plotting
+    x_test = X_test[:,0].reshape((5,20)) + X_train_mean 
+    y_test = X_test[:,1].reshape((5,20)) + X_train_mean 
+    z_pred = z_pred.reshape((5,20)) + z_train_mean 
+
+    x_train = X_train[:,0].reshape((15,20)) + X_train_mean 
+    y_train = X_train[:,1].reshape((15,20)) + X_train_mean 
+    z_tilde = z_tilde.reshape((15,20)) + z_train_mean 
+
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                        linewidth=0, antialiased=False, alpha = 0.7)
+
+    ax.scatter(x_test, y_test, z_pred, color='r', s=10, label='z_Test (Predicted)')
+    ax.scatter(x_train, y_train, z_tilde, color='g', s=10, label='z_Train')
+
+    fig.suptitle('OLS Regression', fontsize=16)
+    ax.set_title(f'Polynomial Degree {deg}', fontsize=10)
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    ax.set_zlim(-0.10, 1.40)
+    ax.legend()
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.savefig('test.png')
+    plt.show()
+
 
 
     
+    # ----------------------------- MSE ---------------------
+    # Reshape z_train and z_test
+    z_test = z_test.reshape((5,20))
+    z_train = z_train.reshape((15,20)) + z_train_mean
+
+
+    MSETrain_OLS = MSE(z_train, z_tilde)
+    MSETest_OLS = MSE(z_test, z_pred)
+
+    R2Train_OLS = R2(z_train, z_tilde)
+    R2Test_OLS = R2(z_test, z_pred)
+
+    print(f'      OLS Regression \n   Polynomial Degree {deg}\n')
+    print(f"Training MSE for OLS = {MSETrain_OLS:10.2e} \nTest MSE OLS = {MSETest_OLS:18.2e} \n")
+    print(f"Training R2 for OLS = {R2Train_OLS:10.2e} \nTest R2 OLS = {R2Test_OLS:18.2e}")
     
+
+
+test_2D()
